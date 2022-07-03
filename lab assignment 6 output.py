038d65bb0a8c1b32a92e8774cd140ed48e01be70
Parth-1011 <103803443+Parth-1011@users.noreply.github.com>
2022-07-03 21:43:40 +0530

    Create lab assignment 6 output.py

---
+++ lab assignment 6 output.py
@@ -0,0 +1,43 @@
+
+Enter a number: 10
+10  is not a perfect number
+
+
+Enter a String: mam
+mam is a palindrome
+
+Enter a number: 6
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+Enter a String: PEC is a great college
+No 'PEC is a great college' is not a panagram
+
+Enter a hyphen separated sentence: red-yellow-blue-green
+blue-green-red-yellow
+
+Student ID= 21102031
+Student ID= 21102031
+Student Name= Parth Anand
+Student ID= 21102031
+Student Name= Parth Anand
+Student Class= 1st Year
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter integers separated by space: 9 1 -10 7 3 4 10
+The triplets that add up to zero are: [[9, 1, -10], [-10, 7, 3]]
+
+Enter the sequence of parantheses : {}[]
+{}[] - is balanced
